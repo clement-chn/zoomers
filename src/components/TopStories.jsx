75525b31603ffef9c53cff8c81d71d6b312e8fb7
Clément Chouan <clement@MacBook-Air-de-Clement.local>
2025-05-10 15:28:38 +0200

    topstories component done

--- src/components/TopStories.jsx
+++ src/components/TopStories.jsx
@@ -4,9 +4,13 @@ import TopStorie from './TopStorie'
 function TopStories() {
   return (
     <>
-      <div className='bg-white mx-auto max-w-7xl flex flex-col justify-center items-center'>
-        <h2 className='self-start'>Top Stories</h2>
-        <TopStorie />
+      <div className='bg-white mx-auto max-w-7xl flex flex-col justify-center items-center mb-6'>
+        <h2 className='self-start text-4xl font-bold m-6'>Top Stories</h2>
+        <div className='flex gap-4 justify-center items-center'>
+          <TopStorie name="Culture Crush" imagePath="vertical_1.jpg"/>
+          <TopStorie name="Trigger Warning" imagePath="vertical_2.jpg" display="sm"/>
+          <TopStorie name="La Flemme De Lire" imagePath="vertical_5.jpg" display="lg"/>
+        </div>
       </div>
     </>
   )
